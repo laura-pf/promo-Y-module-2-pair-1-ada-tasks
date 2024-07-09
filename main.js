6f74166595e2109531b4bbd9723a9b34ecbd47d6
LorenaSTN <lorenstn@hotmail.com>
2024-07-09 11:25:59 +0200

    function handleClick

--- main.js
+++ main.js
@@ -10,16 +10,30 @@ const tasks = [
   { name: "Aprender cómo se realizan las peticiones al servidor en JavaScript", completed: false, id: 4,},
 ];
 
-for(const task of tasks){
+
+function renderTasks () {
+  for(const task of tasks){
 
     if (task.completed === true){
-      taskList.innerHTML += `
-      <li class="border-li line-through">${task.name}</li>`
+      taskList.innerHTML += ` <li class="border-li line-through"><input type="checkbox" checked id="${task.id}"> <span>
+      ${task.name} </span> </li> `;
+
 
      } else {
-      taskList.innerHTML += `
-        <li class="border-li">${task.name}</li>`
+      taskList.innerHTML += `<li class="border-li"> <input type="checkbox" id="${task.id}"> <span> 
+      ${task.name} </span> </li>`
       
      }
+}
 };
+
+renderTasks();
+
+
+function handleClick (event) {
+  
+
+};
+
+taskList.addEventListener("click", handleClick);
 
